29faad67eb723db1f1778f3f41eb92d076555117
zayyo123 <764439554@qq.com>
2023-05-01 21:46:17 +0800

    专为vue开发的监测npm包

--- dist/utils/request.js
+++ dist/utils/request.js
@@ -1,43 +1,34 @@
-/*
- * @Descripttion: 
- * @Author: Wang Dejiang(aei)
- * @Date: 2022-08-07 16:12:27
- * @LastEditors: Wang Dejiang(aei)
- * @LastEditTime: 2022-08-07 16:45:42
- */
-import axios from 'axios'
+import axios from "axios";
 
 // 配置默认URL
 const service = axios.create({
   baseURL:
-    'https://koa-monitorrver-koa-dxsjkmwnnu.cn-hangzhou.fcapp.run/api/mp',
+    "https://koa-monitorrver-koa-dxsjkmwnnu.cn-hangzhou.fcapp.run/api/mp",
   timeout: 15000,
-})
+});
 
 // 添加请求拦截器
 service.interceptors.request.use(
   function (config) {
     // 在发送请求之前做些什么
-    return config
+    return config;
   },
   function (error) {
     // 对请求错误做些什么
-    return Promise.reject(error)
+    return Promise.reject(error);
   }
-)
+);
 
 // 添加响应拦截器
 service.interceptors.response.use(
   function (response) {
     // 对响应数据做点什么
-    return response
+    return response;
   },
   function (error) {
     // 对响应错误做点什么
-    return Promise.reject(error)
+    return Promise.reject(error);
   }
-)
+);
 
-export default service
-
-
+export default service;
