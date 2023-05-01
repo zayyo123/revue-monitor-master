c9bf8c76e847425b2067716fe6f9be7c496f2f81
zayyo123 <764439554@qq.com>
2023-05-01 21:47:46 +0800

    专为vue开发的监测npm包

--- dist/comp/topic.js
+++ dist/comp/topic.js
@@ -1,10 +1,3 @@
-/*
- * @Author: arvin(王德江)
- * @Date: 2022-08-25 20:15:04
- * @LastEditors: arvin(王德江)
- * @LastEditTime: 2022-08-25 20:39:41
- * @Description: 
- */
 class Topic {
   eventList = {}
   on(callback, event) {
