276ed053aef9bc8af85e7551dfcd9820c08d82d2
zayyo123 <764439554@qq.com>
2023-05-03 21:19:51 +0800

    监控sdk

--- dist/comp/bussiness.js
+++ dist/comp/bussiness.js
@@ -1,15 +1,17 @@
+// 主要功能是向服务器发送一pv+uv数据PV (Page Views)，UV (Unique Visitors) 表示独立访客数。 表示页面浏览量。收集我们网站的
 import http from "../utils/request";
 import onload from "../utils/onload";
 
 function business() {
   let connection = navigator.connection;
+  // 在完整加载完成后自动执行
   onload(function () {
     http.post("/plugin/business", {
       kind: "business",
       type: "pv+uv",
-      origin: window.location.origin, // 域名
-      effectiveType: connection.effectiveType, //网络环境
-      timeStamp: Date.now(),
+      origin: window.location.origin, // 当前页面所在的域名
+      effectiveType: connection.effectiveType, //表示设备的网络连接环境
+      timeStamp: Date.now(), //表示当前时间戳
     });
   });
 }
@@ -17,10 +19,12 @@ function business() {
 export default {
   install(Vue, options) {
     const oldRevue = Vue.prototype.$revue;
+    // 使用Object.assign将business方法合并到oldRevue原型对象中。
     Vue.prototype.$revue = Object.assign({}, oldRevue, {
       business,
     });
   },
+  // 安装后立即执行的方法
   immediate: {
     install(Vue, options) {
       business();
@@ -30,5 +34,6 @@ export default {
       });
     },
   },
+  // 还暴露了一个名为bs的方法，用于调用business方法
   bs: business,
 };
