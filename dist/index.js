0d9b9697e84f05cd4f6977c738890e4884d662a1
zayyo123 <764439554@qq.com>
2023-05-03 21:10:42 +0800

    监控sdk

--- dist/index.js
+++ dist/index.js
@@ -1,55 +1,61 @@
-import { timing, ComponentMount, ComponentBlank } from './comp/index' 
-import jsError  from './comp/jsError'
-import wsError from './comp/wsError'
-import httpError, { injectFetch, injectXHR } from './comp/httpError'
-import bussiness from './comp/bussiness'
-import topic from './comp/topic'
+import { timing, ComponentMount, ComponentBlank } from "./comp/index";
+import jsError from "./comp/jsError";
+import wsError from "./comp/wsError";
+import httpError, { injectFetch, injectXHR } from "./comp/httpError";
+import bussiness from "./comp/bussiness";
+import topic from "./comp/topic";
+
+// registLifecallBack函数用于注册生命周期回调函数
 function registLifecallBack(key, callBack) {
-    topic.on(callBack, key)
+  // 将 callBack 注册为一个回调函数，并使用 key 作为主题
+  // 当主题为 key 的事件被触发时，callBack 将会被执行
+  topic.on(callBack, key);
 }
 export default {
+  // install函数用于安装插件并提供全局API
+  install(Vue, options) {
+    // 挂载在Vue.js 的原型中
+    Vue.prototype.$revue = {
+      timing: timing.t, //用于跟踪应用程序中各个组件的渲染和更新时间。
+      compnentMount: ComponentMount.m, //用于跟踪应用程序中各个组件的挂载和卸载时间。
+      blankScreen: ComponentBlank.b, //用于跟踪应用程序是否白屏
+      wsError: wsError, //用于捕获 WebSocket 错误并进行上报
+      injectFetch, //用于捕获和监控 fetch 请求
+      injectXHR, //用于捕获和监控 XMLHttpRequest 请求
+      bussiness, //用于记录和跟踪应用程序中的业务逻辑
+    };
+  },
+  // immediate函数用于立即执行插件的初始化代码
+  immediate: {
     install(Vue, options) {
-        Vue.prototype.$revue = {
-          timing: timing.t,
-          compnentMount: ComponentMount.m,
-          blankScreen: ComponentBlank.b,
-          wsError: wsError,
-          injectFetch, 
-          injectXHR,
-          bussiness
-        }
+      ComponentBlank.b();
+      timing.t(Vue, options);
+      Vue.mixin(ComponentMount.m);
+      jsError.install(Vue);
+      wsError();
+      injectFetch();
+      injectXHR();
+      bussiness.bs();
+      Vue.prototype.$revue = {
+        timing: timing.t,
+        compnentMount: ComponentMount.m,
+        blankScreen: ComponentBlank.b,
+        wsError: wsError,
+        injectFetch,
+        injectXHR,
+        bussiness,
+      };
     },
-    immediate: {
-        install(Vue, options) {
-          ComponentBlank.b() 
-          timing.t(Vue, options)
-          Vue.mixin(ComponentMount.m)
-          jsError.install(Vue)
-          wsError()
-          injectFetch()
-          injectXHR()
-          bussiness.bs()
-          Vue.prototype.$revue = {
-            timing: timing.t,
-            compnentMount: ComponentMount.m,
-            blankScreen: ComponentBlank.b,
-            wsError: wsError,
-            injectFetch, 
-            injectXHR,
-            bussiness
-          }
-        },
-    },
-    registLifecallBack
-}
+  },
+  registLifecallBack,
+};
 
 export {
-    timing,
-    ComponentMount,
-    ComponentBlank,
-    jsError,
-    wsError,
-    httpError,
-    topic
-}
-
+  timing,
+  ComponentMount,
+  ComponentBlank,
+  jsError,
+  wsError,
+  httpError,
+  topic,
+};
--- dist/utils/getLastEvent.js
+++ dist/utils/getLastEvent.js
@@ -1,19 +1,24 @@
-let lastEvent
-;['click', 'touchstart', 'mousedown', 'keydown', 'mouseover'].forEach(
+// 该函数返回最近一次的用户事件对象，例如点击、触摸、鼠标按下、键盘按下或鼠标移动事件对象。
+
+// 先定义了一个 lastEvent 变量用于存储最近一次的事件对象
+let lastEvent;
+// 然后通过 forEach 方法遍历了包含了五个不同事件类型的数组，并通过 document.addEventListener 方法给 document 对象添加事件监听器然后通过 forEach 方法遍历了包含了五个不同事件类型的数组，并通过 document.addEventListener 方法给 document 对象添加事件监听器
+["click", "touchstart", "mousedown", "keydown", "mouseover"].forEach(
   (eventType) => {
     document.addEventListener(
       eventType,
       (event) => {
-        lastEvent = event
+        // 将事件对象赋值给 lastEvent 变量，以记录最近一次的事件对象
+        lastEvent = event;
       },
       {
-        capture: true, //捕获阶段
+        capture: true, //表示在捕获阶段触发事件监听器
         passive: true, //默认不阻止默认事件
       }
-    )
+    );
   }
-)
+);
 
 export default function () {
-  return lastEvent
+  return lastEvent;
 }
