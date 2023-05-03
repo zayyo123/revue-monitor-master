91a90b13ef237b77ccd5c9b9c2b4b104111e465f
zayyo123 <764439554@qq.com>
2023-05-03 15:41:01 +0800

    监控中台

--- dist/utils/tracker.js
+++ dist/utils/tracker.js
@@ -1,9 +1,13 @@
 import axios from "axios";
 
+// 用axios实例的create方法创建网络请求
 const service = axios.create({
+  // 请求的共同URL部分
   baseURL:
     "https://koa-monitorrver-koa-dxsjkmwnnu.cn-hangzhou.fcapp.run/api/mp",
+    // 请求超时时间
   timeout: 15000,
+  // 请求头的设置
   headers: {
     Accept: "application/json",
     "Conetent-Type": "application/json",
@@ -12,9 +16,9 @@ const service = axios.create({
 
 export default {
   postHTTP(data = {}) {
-    //console.log("log", data);
+    // 发生一个post请求
+    // 在baseURL的基础上拼接上"/plugin/http"
     return service.post("/plugin/http", data).then((response) => {
-      //console.log("发送成功", response);
     });
   },
 };
