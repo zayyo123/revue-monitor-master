41d1be1522b3962668f5c0605ab08c8480b4834c
zayyo123 <764439554@qq.com>
2023-05-04 15:29:25 +0800

    监控sdk

--- dist/comp/topic.js
+++ dist/comp/topic.js
@@ -1,15 +1,18 @@
 class Topic {
-  eventList = {}
+  eventList = {};
+  // 订阅回调函数
   on(callback, event) {
-    ;(this.eventList[event] || (this.eventList[event] = [])).push(callback)
+    (this.eventList[event] || (this.eventList[event] = [])).push(callback);
   }
+  // 触发指定事件并执行相应的回调函数
   emit(event, ...arr) {
-    this.eventList[event] && this.eventList[event].forEach(e => {
-      e(...arr)
-    });
+    this.eventList[event] &&
+      this.eventList[event].forEach((e) => {
+        e(...arr);
+      });
   }
 }
 
-const topic = new Topic()
+const topic = new Topic();
 
-export default topic
+export default topic;
--- dist/comp/wsError.js
+++ dist/comp/wsError.js
@@ -1,59 +1,58 @@
-
 let userAgent = require("user-agent");
-const xhr___ = new XMLHttpRequest()
+const xhr___ = new XMLHttpRequest();
 const monitor = () => {
-  WebSocket.prototype.flag_ = false
-  WebSocket.prototype.oldsend = WebSocket.prototype.send
+  WebSocket.prototype.flag_ = false;
+  WebSocket.prototype.oldsend = WebSocket.prototype.send;
   WebSocket.prototype.send = function (data) {
-    WebSocket.prototype.flag_ = false
-    WebSocket.prototype.startTime = new Date()
-    WebSocket.prototype.oldsend.call(this, data)
-  }
-  WebSocket.prototype.oldclose = WebSocket.prototype.close
+    WebSocket.prototype.flag_ = false;
+    WebSocket.prototype.startTime = new Date();
+    WebSocket.prototype.oldsend.call(this, data);
+  };
+  WebSocket.prototype.oldclose = WebSocket.prototype.close;
   WebSocket.prototype.close = function (err) {
     if (!WebSocket.prototype.flag_) {
       xhr___.open(
-        'POST',
-        'https://koa-monitorrver-koa-dxsjkmwnnu.cn-hangzhou.fcapp.run/api/mp/plugin/postSocket'
-      )
-      xhr___.setRequestHeader('Content-Type', 'application/json')
+        "POST",
+        "https://koa-monitorrver-koa-dxsjkmwnnu.cn-hangzhou.fcapp.run/api/mp/plugin/postSocket"
+      );
+      xhr___.setRequestHeader("Content-Type", "application/json");
       xhr___.send(
         JSON.stringify({
           title: document.title,
           url: location.href,
           timeStamp: new Date().getTime(),
           userAgent: userAgent.parse(navigator.userAgent).fullName,
-          kind: 'stability',
-          type: 'WebSocket',
-          eventType: 'WebSocketClose',
+          kind: "stability",
+          type: "WebSocket",
+          eventType: "WebSocketClose",
           pathName: location.pathname,
           duration: new Date() - WebSocket.prototype.startTime,
           statusCode: 1011,
-          method: 'WebSocket',
+          method: "WebSocket",
         })
-      )
-      WebSocket.prototype.oldclose.call(this)
+      );
+      WebSocket.prototype.oldclose.call(this);
     }
-    WebSocket.prototype.flag_ = true
-  }
-}
+    WebSocket.prototype.flag_ = true;
+  };
+};
 
 export const reWsError = {
   install() {
-    const oldRevue = Vue.prototype.$revue
+    const oldRevue = Vue.prototype.$revue;
     Vue.prototype.$revue = Object.assign({}, oldRevue, {
-      monitor
-    })
+      monitor,
+    });
   },
   immediate: {
     install() {
-      const oldRevue = Vue.prototype.$revue
+      const oldRevue = Vue.prototype.$revue;
       Vue.prototype.$revue = Object.assign({}, oldRevue, {
-        monitor
-      })
-      monitor()
-    }
+        monitor,
+      });
+      monitor();
+    },
   },
-}
+};
 
-export default monitor
+export default monitor;
